0b66e65b524bd6435949c779e1202cd9d007f4fa
Stefan Gründel <stefan.gruendel@garmin.com>
2018-04-11 09:55:11 +0200

    Remove aws-lambda-mock-context dependency for tests.

--- test/radio-gong-intent.js
+++ test/radio-gong-intent.js
@@ -1,44 +1,51 @@
 'use strict';
 
-var expect = require('chai').expect;
-var index = require('../src/index');
+const expect = require('chai').expect;
+const index = require('../src/index');
 
-const context = require('aws-lambda-mock-context');
-const ctx = context();
+const event = {
+    session: {
+        sessionId: 'SessionId.154291c5-a13f-4e7a-ab5a-2342534adfeba',
+        application: {
+            applicationId: 'amzn1.ask.skill.8b0359cd-df17-46f0-b1fa-509d6e9ca1cc',
+        },
+        attributes: {},
+        user: {
+            userId: 'amzn1.ask.account.[unique-value-here]',
+        },
+        new: true,
+    },
+    request: {
+        type: 'IntentRequest',
+        requestId: 'amzn1.echo-api.request.[unique-value-here]',
+        locale: 'de-DE',
+        timestamp: '2017-03-30T13:02:01Z',
+        intent: {
+            name: 'RadioGongIntent',
+            slots: {},
+        },
+    },
+    version: '1.0',
+};
 
 describe('Testing a session with the RadioGongIntent:', () => {
     var speechResponse = null;
     var speechError = null;
 
-    before(function(done) {
-        index.handler({
-            session: {
-                sessionId: 'SessionId.154291c5-a13f-4e7a-ab5a-2342534adfeba',
-                application: {
-                    applicationId: 'amzn1.ask.skill.8b0359cd-df17-46f0-b1fa-509d6e9ca1cc',
-                },
-                attributes: {},
-                user: {
-                    userId: 'amzn1.ask.account.[unique-value-here]',
-                },
-                new: true,
-            },
-            request: {
-                type: 'IntentRequest',
-                requestId: 'amzn1.echo-api.request.[unique-value-here]',
-                locale: 'de-DE',
-                timestamp: '2017-03-30T13:02:01Z',
-                intent: {
-                    name: 'RadioGongIntent',
-                    slots: {},
-                },
-            },
-            version: '1.0',
-        }, ctx);
-
-        ctx.Promise
-            .then(resp => { speechResponse = resp; done(); })
-            .catch(err => { speechError = err; done(); });
+    before(function() {
+        return new Promise((resolve, reject) => {
+            index.handler(event,
+                null,
+                (err, resp) => {
+                    if (err) {
+                        speechError = err;
+                        reject(err);
+                    } else {
+                        speechResponse = resp;
+                        resolve(speechResponse);
+                    }
+                });
+        });
     });
 
     describe('The response', () => {
